40007062200d50c8562de5f88a1fd2d4f2eb020f
Harrison Leon <harrisonleon02@gmail.com>
2014-10-26 07:55:33 -0700

    Removed comment block
    Removed comment block; minor changes

--- src/Document.java
+++ src/Document.java
@@ -1,8 +1,7 @@
-/* import org.apache.poi.hwpf.Word2Forrest;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-*/
+
 /*
  * Make sure to add the jar files to the classPath!!
  * 
@@ -18,10 +17,10 @@ C:\Users\ramag_000\Downloads\poi-bin-3.10.1-20140818\poi-3.10.1\ooxml-lib\xmlbea
 
  */
 
-/* import java.io.*;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import java.util.Scanner;
+
 
 public class Document
 {
@@ -67,7 +66,7 @@ public class Document
                 wordExtractor.close();
             } catch (IOException e)
             {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             }
         }
@@ -79,4 +78,4 @@ public class Document
     }
 }
 
-*/
+
